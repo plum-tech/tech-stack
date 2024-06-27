0683ae945dc10766605a5e7cca53152f8737362d
Liplum <Li_plum@outlook.com>
2024-06-27 21:22:11 +0800

    updated config and workflows

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -74,7 +74,7 @@ const config: Config = {
         },
         { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/liplum-dev/tech-stack',
           label: 'GitHub',
           position: 'right',
         },
